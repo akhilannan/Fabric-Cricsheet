6a69bb737cc3d4cc46add3300ccd221d666ca1fc
akhilannan <97329617+akhilannan@users.noreply.github.com>
2024-02-12 17:57:07 +0530

    updates to code

--- Notebooks/Cricsheet Model Refresh.py
+++ Notebooks/Cricsheet Model Refresh.py
@@ -18,7 +18,7 @@ get_ipython().run_line_magic('run', '"/Common Functions"')
 # In[ ]:
 
 
-dataset_lists = ["Cricsheet Model"]
+dataset_list = "Cricsheet Model" # Pass multiple coma seperated "model1,model2"
 
 
 # # Refresh and Wait for Dataset completion
@@ -26,5 +26,9 @@ dataset_lists = ["Cricsheet Model"]
 # In[ ]:
 
 
-refresh_and_wait(dataset_lists)
+# Split the dataset_list string by commas and store the result as a list
+dataset_list = dataset_list.split(",")
+
+# Start the dataset refresh and wait for completion
+refresh_and_wait(dataset_list)
 
